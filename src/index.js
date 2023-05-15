3f81c917efa0773b3a163298c4ff7a9012dff874
Bruno H Cardoso <riquebruno@yahoo.com.br>
2023-05-15 15:52:30 -0300

    Iniciando o Requisito 01

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+// Iniciando
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(

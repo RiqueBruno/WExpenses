9b93ea08e0588ca3e62dcb8adfba77399d1effa0
Bruno H Cardoso <riquebruno@yahoo.com.br>
2023-05-16 17:54:49 -0300

    Requisito 1 Ok.

---
+++ src/pages/Login.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { userEmail } from '../redux/actions';
+
+class Login extends React.Component {
+  state = {
+    btnIsDisabled: true,
+    password: '',
+    email: '',
+  };
+
+  handlerChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    }, this.verify);
+  };
+
+  verify = () => {
+    const { email, password } = this.state;
+    const min = 6;
+    const passwordVerify = password.length >= min;
+    const emailVerify = /\S+@\S+\.\S+/.test(email);
+
+    this.setState({
+      btnIsDisabled: !(passwordVerify && emailVerify),
+    });
+  };
+
+  handlerClick = () => {
+    const { email } = this.state;
+    const { dispatch, history } = this.props;
+
+    dispatch(userEmail(email));
+    history.push('/carteira');
+  };
+
+  render() {
+    const { btnIsDisabled } = this.state;
+    return (
+      <article>
+        <input
+          type="text"
+          name="email"
+          data-testid="email-input"
+          onChange={ this.handlerChange }
+        />
+        <input
+          type="password"
+          name="password"
+          data-testid="password-input"
+          onChange={ this.handlerChange }
+        />
+        <button
+          type="button"
+          name="btnIsActive"
+          disabled={ btnIsDisabled }
+          onClick={ this.handlerClick }
+        >
+          Entrar
+        </button>
+      </article>
+    );
+  }
+}
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);

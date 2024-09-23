22259cc24eaf1234287acba723d3089c68db9fea
Bruno H <riquebruno@yahoo.com.br>
2024-09-23 12:40:57 -0300

    refactor: atualiza configurações de tailwind adicionando cores padrões ao projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
+    colors: {
+      primary: '#F98C29',
+      secondary: '#FFD600',
+      tertiary: '#FF2305',
+      quaternary: '#8205FF',
+    },
+    backgroundImage: {
+      'bg-btn-gradient': 'linear-gradient(to bottom, #FFD600 0%, #F98C29 64%, #FF2305 91%, #8205FF 100%)',
+    },
   },
   plugins: [],
 };

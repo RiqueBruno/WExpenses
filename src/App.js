59acbb76178510be6b394f78f7063c3919ea6b3a
Bruno H <riquebruno@yahoo.com.br>
2024-09-23 11:48:36 -0300

    feat: adiciona rota * para paginas não encontradas

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/carteira" component={ Wallet } />
+        <Route path="*" component={ NotFound } />
       </Switch>
     </div>
   );

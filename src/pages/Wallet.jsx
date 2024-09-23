3dc089c09a2ecdc4e00427ee57c2dc84ce60d365
Bruno H <riquebruno@yahoo.com.br>
2024-09-23 16:58:47 -0300

    refactor: retira div não utilizada na page Wallet

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -7,10 +7,9 @@ class Wallet extends React.Component {
   render() {
     return (
       <div>
-        <div className="divBlur" />
         <div className="divMain">
+          <Header />
           <div className="divMainHeader">
-            <Header />
             <WalletForm />
           </div>
           <Table />

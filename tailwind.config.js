f219e8391fe67156872e23a6868b2ffca5763176
Bruno H <riquebruno@yahoo.com.br>
2024-09-23 22:14:36 -0300

    refactor: add slide-down animation to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-down': {
+          '0%': {
+            transform: 'translateY(-100%)', // Começa fora da tela
+          },
+          '100%': {
+            transform: 'translateY(0)', // Fica na posição original
+          },
+        },
+      },
+      animation: {
+        'slide-down': 'slide-down 0.5s ease-out forwards', // Define a animação
+      },
+    },
     colors: {
       primary: '#F98C29',
       secondary: '#FFD600',
@@ -12,7 +26,7 @@ module.exports = {
       black: '#000000',
       gray: '#1A1B1C',
       grayOrangeLight: '#ffb55e',
-      orangeTranparent: '361C0599',
+      orangeTranparent: '#361C0599',
     },
     backgroundImage: {
       'bg-btn-gradient':
